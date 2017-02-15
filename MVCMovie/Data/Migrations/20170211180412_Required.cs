56d2d93e72ca1d19c17f33463cbff0d902c4697d
jmurphy49 <jmurphy49@W766294>
2017-02-14 19:37:22 -0700

    Initial project upload

---
+++ MVCMovie/Data/Migrations/20170211180412_Required.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVCMovie.Data.Migrations
+{
+    public partial class Required : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movie",
+                nullable: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movie",
+                nullable: true);
+        }
+    }
+}
---
+++ MVCMovie/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Display(Name = "Release Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+
+
+
+    }
+}
